b3f78ce3c9af1350170f6f90ee27f24802b70d7e
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-01 16:01:22 +0100

    add add todo form

---
+++ tomatodo/components/AddTodoForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const AddTodoForm = ({ setAddTodoFormIsVisible, currentListDocId}) => {
+	const [userInputName, setUserInputName] = useState('');
+	const [userInputText, setUserInputText] = useState('');
+	const [userInputTime, setUserInputTime] = useState(0);
+	const [errMessage, setErrMessage] = useState('');
+
+	const handleSubmit = async (e) => {
+		if (userInputName.length == 0 ) {
+			setErrMessage("Namnet måste vara minst 1 tecken lång.");
+		} else {
+		  await fetch("/api/todos/todo", {
+			method: "POST",
+			body: JSON.stringify({
+				name: userInputName,
+				description: userInputText,
+				time: userInputTime,
+				parentRef: currentListDocId,
+			}),
+		  });
+		  setUserInputName("");
+		setUserInputTime(0);
+		setUserInputText("");
+		setErrMessage("");
+		}
+	  };
+
+	return (
+		<article>
+			<h1>Mina listor</h1>
+			<input type="text" placeholder="Namn på todo" onChange={(e) => setUserInputName(e.target.value)} />
+			<input type="text" placeholder="Beskrivning" onChange={(e) => setUserInputText(e.target.value)} />
+			<input type="number" placeholder="Tid" onChange={(e) => setUserInputTime(e.target.value)}/>
+			<input type="button" value="lägg till" onClick={() => handleSubmit()} />
+			{errMessage}
+			<button onClick={() => setAddTodoFormIsVisible(false)}>stäng</button>
+		</article>
+	)
+};
+
+export default AddTodoForm;

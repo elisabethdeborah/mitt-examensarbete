a77d6c6094ab3d92eb81233dbf51c1927403964f
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-02 16:22:41 +0100

    add sparade-listor page

---
+++ tomatodo/pages/sparade-listor.jsx
@@ -0,0 +1,12 @@
+import Meta from "../components/Meta";
+
+const SparadeListor = () => (
+	<div>
+		<Meta title='Mina sparade listor' />
+		<h1>
+			Mina sparade listor
+		</h1>
+	</div>
+);
+
+export default SparadeListor;

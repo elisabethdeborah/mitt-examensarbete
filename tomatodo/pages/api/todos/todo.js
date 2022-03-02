d2831506fb3a1d33451f75aa13b11031d5c80a61
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-03 00:53:00 +0100

    add PUT and DELETE to todo route

--- tomatodo/pages/api/todos/todo.js
+++ tomatodo/pages/api/todos/todo.js
@@ -1,32 +1,67 @@
 import client from "../../../lib/sanity";
 
 const todo = async function handler(req, res) {
-	console.log('client in api page', 'client', req.body)
-      const newTodo = await JSON.parse(req.body);
-      try {
-        await client
-          .create({
-            _type: "todo",
-            title: newTodo.name,
-			description: newTodo.description,
-			time: Number(newTodo.time),
-			checked: false,
-            publishedAt: new Date().toISOString(),
-			todoList: {
-				_type: 'reference',
-				_ref: `${newTodo.parentRef}`
+	switch (req.method) {
+		case "POST":
+			const newTodo = await JSON.parse(req.body);
+			try {
+				await client
+				.create({
+					_type: "todo",
+					title: newTodo.name,
+					description: newTodo.description,
+					time: Number(newTodo.time),
+					checked: false,
+					publishedAt: new Date().toISOString(),
+					todoList: {
+						_type: 'reference',
+						_ref: `${newTodo.parentRef}`
+					}
+				})
+				.then((res) => {
+					console.log(`Todo was created, document ID is ${res._id}. Res body is ${res.body}`);
+				});
+					res
+					.status(200)
+					.json({ msg: `Todo was created, document ID is ${res._id}` });
+			} catch (error) {
+					console.error(error);
+					res.status(500).json({ msg: "Error, check console" });
 			}
-          })
-          .then((res) => {
-            console.log(`Todo was created, document ID is ${res._id}. Res body is ${res.body}`);
-          });
-        res
-          .status(200)
-          .json({ msg: `Todo was created, document ID is ${res._id}` });
-      } catch (err) {
-        console.error(err);
-        res.status(500).json({ msg: "Error, check console" });
-  }
+			break;
+
+		case "PUT":
+			try {
+				const result = await client
+				.patch(req.body.id)
+				.set({
+					checked: req.body.checked,
+				})
+				.commit();
+				res.status(200).json({
+					status: result.checked,
+				});
+			} catch (error) {
+				console.error(error);
+					res.status(500).json({ msg: "Error, check console" });
+			}
+			break;
+
+		case "DELETE":
+			await client
+				.delete(req.body)
+				.then((res) => {
+				res.body;
+				})
+				.then((res) => console.log(`Todo was deleted`));
+			res.status(200).json({ msg: "Success" });
+
+	
+		default:
+			break;
+	}
+	console.log('client in todos/todo api page', req.body)
+      
 }
 
 export default todo;

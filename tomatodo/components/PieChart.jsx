5fed0dcea3c231839f8cbb121b20327c5ac6ae26
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-02 16:21:16 +0100

    add piechart component

---
+++ tomatodo/components/PieChart.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Pie} from 'react-chartjs-2';
+import {Chart, ArcElement} from 'chart.js'
+import Blast from '../svgAssets/newtomato-whiteborder-blast3.svg';
+import styles from '../styles/charts.module.scss';
+import clsx from 'clsx';
+Chart.register(ArcElement);
+
+
+
+const TimerChart = ({startTime, timeLeft, color}) => {
+	const transparent = startTime-timeLeft;
+	const orange = timeLeft;
+	const fill = color === "green" ? "yellow": '#FF9100';
+	const state = {
+		datasets: [
+		  {
+			backgroundColor: [
+				'transparent',
+				`${fill}`,
+			],
+			borderWidth: [
+				0,
+				3,
+			],
+			data: [transparent, orange]
+		  }
+		],
+	  }
+	
+	  return (
+		<div className={styles.pieChartWrapper}>
+			<Blast className={clsx(styles.chartBlast, {[styles.animate]: startTime})} />
+			<Pie
+			className={styles.pieChartObj}
+				data={state}
+			/>
+		</div>
+	  );
+  };
+
+  export default TimerChart;

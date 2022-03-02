8a9661b0c2d76dcfd569c1610e942d16814df3e0
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-02 21:22:28 +0100

    add active nav link styling

--- tomatodo/components/Navigation.jsx
+++ tomatodo/components/Navigation.jsx
@@ -2,18 +2,36 @@
 import clsx from "clsx";
 import React, { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from '../styles/Navigation.module.scss';
 
 const Navigation = ({navIsOpen, setNavIsOpen}) => {
+	const router = useRouter();
+
 	return (
 		<nav className={clsx(styles.navContainer, {[styles.navIsOpen]: navIsOpen})}>
 			<ul onClick={() => setNavIsOpen()} className={styles.list}>
-				<li className={styles.navLink}><Link href='/' className>Start</Link></li>
-				<li className={styles.navLink}><Link href='/sparade-listor'>Mina sparade listor</Link></li>
-				<li className={styles.navLink}><Link href='/timer'>Timer</Link></li>
-				<li className={styles.navLink}><Link href='/mina-tomater'>Mina tomater</Link></li>
-				<li className={styles.navLink}><Link href='/mina-todos'>Mina todos</Link></li>
-				<li className={styles.navLink}><Link href='/settings'>Inställningar</Link></li>
+				<li className={clsx(styles.navLink, {[styles.active]: router.pathname=='/'})}>
+					<Link href='/'>Start</Link>
+				</li>
+				<li className={clsx(styles.navLink, {[styles.active]: router.pathname=='/sparade-listor'})}>
+					<Link href='/sparade-listor'>Mina sparade listor</Link>
+				</li>
+				<li className={clsx(styles.navLink, {[styles.active]: router.pathname=='/timer'})}>
+					<Link href='/timer'>Timer</Link>
+				</li>
+				<li className={clsx(styles.navLink, {[styles.active]: router.pathname=='/stopwatch'})}>
+					<Link href='/stopwatch'>Stopwatch</Link>
+				</li>
+				<li className={clsx(styles.navLink, {[styles.active]: router.pathname=='/mina-tomater'})}>
+					<Link href='/mina-tomater'>Mina tomater</Link>
+				</li>
+				<li className={clsx(styles.navLink, {[styles.active]: router.pathname=='/mina-todos'})}>
+					<Link href='/mina-todos'>Mina todos</Link>
+				</li>
+				<li className={clsx(styles.navLink, {[styles.active]: router.pathname=='/settings'})}>
+					<Link href='/settings'>Inställningar</Link>
+				</li>
 			</ul>
 		</nav>
 	)

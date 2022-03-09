da8ecdcf484e653eda53bad88de32e3dfdb0f6ca
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-09 09:36:10 +0100

    change header img to text

--- tomatodo/components/Header.jsx
+++ tomatodo/components/Header.jsx
@@ -3,7 +3,7 @@ import clsx from "clsx";
 import styles from '../styles/Header.module.scss';
 import Navigation from './Navigation';
 import Hamburger from '../svgAssets/hamburger.svg';
-import CloseNav from '../svgAssets/closeNav.svg';
+import CloseNav from '../svgAssets/deleteTomatoWhiteNew.svg';
 const Header = () => {
 	const [navIsOpen, setNavIsOpen] = useState(false);
 	return (
@@ -11,6 +11,10 @@ const Header = () => {
 		<CloseNav className={clsx(styles.closeNavBtn, {[styles.showClosebtn]: navIsOpen})} onClick={() => setNavIsOpen(false)} />
 		<Hamburger className={clsx(styles.hamburger, {[styles.showHamburger]: !navIsOpen})} onClick={() => setNavIsOpen(true)} />
 			<section className={clsx(styles.logoContainer, {[styles.navIsOpen]: navIsOpen})} />
+			<section className={styles.headerTextContainer}>
+				<h1 className={clsx(styles.logoHeader, styles.partOne)}>toma</h1>
+				<h1 className={clsx(styles.logoHeader, styles.partTwo)}>todo</h1>
+			</section>
 			<Navigation navIsOpen={navIsOpen} setNavIsOpen={setNavIsOpen} />
 		</header>
 	)

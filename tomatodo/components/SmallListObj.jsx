99a1a2d3377d25ae17020bf08c1d567dc9846dae
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-10 19:40:50 +0100

    change layout, update css

--- tomatodo/components/SmallListObj.jsx
+++ tomatodo/components/SmallListObj.jsx
@@ -1,5 +1,4 @@
 
-import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from '../styles/smallListObj.module.scss';
 import clsx from 'clsx';
@@ -7,28 +6,24 @@ import NumberFormat from './NumberFormat';
 import PlayTimerBtn from './PlayTimerBtn';
 
 
-const SmallListObj = (listItem, handleClick, clickParam) => {
-	console.log('small list obj', listItem)
+const SmallListObj = (listItem, handleClick, contentIsVisible) => {
+	//console.log('listItem', listItem)
 	return (
 		<>
-		<article onClick={() => handleClick(clickParam)} className={styles.hiddenLists}>
-			{
-			listItem._type === "tomato"? (
-				<>
-				<article className={styles.smallTomato} />
-				<section className={styles.textGroup}>
-					<h3>{listItem.title}</h3>
-					<div className={styles.tomatoTime}>
-						<NumberFormat timeSeconds={tomato.time} text={'tid: '} textSize={'0.75rem'} /></div>
-				</section>
-				</>
-			):(	
-				<section className={styles.textGroup}>
-					<h3>{listItem.name}</h3>
-					{listItem && listItem._createdAt && (<p>tillagd: {listItem._createdAt.slice(0, 10)}</p>)}
-				</section>
-			)
-			}
+		<article onClick={() => handleClick()} className={clsx(
+			styles.hiddenLists, 
+			{[styles.isVisible] : contentIsVisible === true})}>
+			<>
+			{listItem._type === "tomato" && <article className={styles.smallTomato} />}
+			<section className={styles.textGroup}>
+				<h3>{listItem.title}</h3>
+				{listItem._type === "tomato"? (
+				<div className={styles.tomatoTime}>
+					<NumberFormat milliSeconds={tomato.time*1000} text={'tid: '} textSize={'0.75rem'} />
+				</div>)
+				: listItem && listItem._createdAt && (<p>tillagd: {listItem._createdAt.slice(0, 10)}</p>)}
+			</section>
+			</>
 			<PlayTimerBtn listItem={listItem} />
 		</article>
 		</>

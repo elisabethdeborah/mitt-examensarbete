5aa00a47bfebb3f884de014727ce7e6f9ccb2bf2
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-11 17:45:27 +0100

    make list objects set current state

--- tomatodo/components/SmallListObj.jsx
+++ tomatodo/components/SmallListObj.jsx
@@ -4,16 +4,16 @@ import styles from '../styles/smallListObj.module.scss';
 import clsx from 'clsx';
 import NumberFormat from './NumberFormat';
 import PlayTimerBtn from './PlayTimerBtn';
+import {useUpdateContext, useTodoContext} from "../context/TodoContext";
 
-
-const SmallListObj = (listItem, handleClick, contentIsVisible) => {
-	//console.log('listItem', listItem)
+const SmallListObj = (listItem, contentIsVisible) => {
+	const currentState = useUpdateContext();
+	const handleClick = () => console.log('currentState:', currentState)
 	return (
-		<>
 		<article onClick={() => handleClick()} className={clsx(
 			styles.hiddenLists, 
 			{[styles.isVisible] : contentIsVisible === true})}>
-			<>
+			<div onClick={() => currentState.setCurrentItem(listItem)}>
 			{listItem._type === "tomato" && <article className={styles.smallTomato} />}
 			<section className={styles.textGroup}>
 				<h3>{listItem.title}</h3>
@@ -23,10 +23,9 @@ const SmallListObj = (listItem, handleClick, contentIsVisible) => {
 				</div>)
 				: listItem && listItem._createdAt && (<p>tillagd: {listItem._createdAt.slice(0, 10)}</p>)}
 			</section>
-			</>
+			</div>
 			<PlayTimerBtn listItem={listItem} />
 		</article>
-		</>
 	)
 }
 

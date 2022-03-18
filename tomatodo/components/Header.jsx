e0234eceebffacb4be8588fc155e3342da5f337c
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-18 01:15:15 +0100

    add link to home page to logo

--- tomatodo/components/Header.jsx
+++ tomatodo/components/Header.jsx
@@ -3,20 +3,24 @@ import clsx from "clsx";
 import styles from '../styles/Header.module.scss';
 import Navigation from './Navigation';
 import Hamburger from '../svgAssets/hamburger.svg';
+import Link from 'next/link';
 import CloseNav from '../svgAssets/deleteTomatoWhiteNew.svg';
+
 const Header = () => {
 	const [navIsOpen, setNavIsOpen] = useState(false);
 	return (
 		<header className={styles.header}>
-		<CloseNav className={clsx(styles.closeNavBtn, {[styles.showClosebtn]: navIsOpen})} onClick={() => setNavIsOpen(false)} />
-		<Hamburger className={clsx(styles.hamburger, {[styles.showHamburger]: !navIsOpen})} onClick={() => setNavIsOpen(true)} />
+			<CloseNav className={clsx(styles.closeNavBtn, {[styles.showClosebtn]: navIsOpen})} onClick={() => setNavIsOpen(false)} />
+			<Hamburger className={clsx(styles.hamburger, {[styles.showHamburger]: !navIsOpen})} onClick={() => setNavIsOpen(true)} />
 			<section className={clsx(styles.logoContainer, {[styles.navIsOpen]: navIsOpen})} />
-			<section className={styles.headerTextContainer}>
-				<h1 className={clsx(styles.logoHeader, styles.partOne)}>toma</h1>
-				<h1 className={clsx(styles.logoHeader, styles.partTwo)}>todo</h1>
-			</section>
+			<Link href={'/'} passHref>
+				<section className={styles.headerTextContainer}>
+					<h1 className={clsx(styles.logoHeader, styles.partOne)}>toma</h1>
+					<h1 className={clsx(styles.logoHeader, styles.partTwo)}>todo</h1>
+				</section>
+			</Link>
 			<Navigation navIsOpen={navIsOpen} setNavIsOpen={setNavIsOpen} />
 		</header>
-	)
+)
 };
 export default Header;

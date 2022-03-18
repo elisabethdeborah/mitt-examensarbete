22f934a1c108ccdb7dd3f0442aa005b54a216f3e
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-18 23:01:07 +0100

    add currentlists-list, replace smallinputobj, update onClick, css

--- tomatodo/components/SavedLists.jsx
+++ tomatodo/components/SavedLists.jsx
@@ -3,13 +3,38 @@ import Link from 'next/link';
 import styles from '../styles/savedLists.module.scss';
 import clsx from 'clsx';
 import SmallListObj from './smallListObj';
+import ListContainer from "../components/ListContainer";
+import ActiveLists from "../components/ActiveLists";
 
-const SavedLists = ({lista, setSideListsVisible, setOpen, page}) => {
+const SavedLists = ({lista, setSideListsVisible, setOpen, page, activeLists, setAddListFormIsVisible }) => {
 	const [contentIsVisible, setContentIsVisible] = useState(false);
-	const [popupIsOpen, setPoputIsOpen] = useState(false);
+	const [popupIsOpen, setPopupIsOpen] = useState(false);
+	const [previewTodos, setPreviewTodos] = useState(false);
+	const [previewTodosList, setPreviewTodosList] = useState(null);
 
-	const handleClick = (x) => {
-		setOpen(x)
+	const handleClick = (list, index) => {
+		if (page === 'todo') {
+			console.log('lägg till, sätt denna som open ', activeLists.length-1, index)
+			setOpen(activeLists.length-1 + index)
+		 } else if (page === 'home') {
+			console.log('go to todolists med denna som open: ', list) 
+		 }
+	}
+
+	const handleClickOpen = (list, index) => {
+		//setAddListFormIsVisible(true)
+		if (page === 'todo') {
+			//setPreviewTodos(true);
+			/* list.todos? 
+			setPreviewTodosList(null):setPreviewTodosList(list.todos); */
+			setPreviewTodosList(list.todos)
+			console.log("show todolist's todos: ", list.todos, 'index', index) 
+			//console.log('lägg till, sätt denna som open ', activeLists.length-1, index)
+			//setOpen(activeLists.length-1 + index)
+		 } else if (page === 'home') {
+			console.log("show todolist's todos: ", list.todos) 
+		 }
+		 
 	}
 
 	return (
@@ -27,18 +52,45 @@ const SavedLists = ({lista, setSideListsVisible, setOpen, page}) => {
 				</Link>
 			{
 				lista ? lista.map((list, index) => {
+					let listItem = previewTodosList? list.todos:null;
+					console.group('listItem', listItem, 'index', index)
 					return (
-						<>
+						<div key={index}>
+						{previewTodosList &&(
+							listItem? (
+								listItem.map((todoItem) =>(
+							<article key={todoItem._rev} onClick={() => setPopupIsOpen(true)} className={styles.hiddenLists}>
+								<section className={styles.textGroup}>
+									<h3>{todoItem.title}</h3>
+									{todoItem.description && (<p>Beskrivning: {`${todoItem.description}`}</p>)}
+								</section>
+							</article>
+								))
+							
+							):
+							<h2>Den här listan är tom</h2>
+						)}
 						{
 							popupIsOpen && (
 								<section className={styles.popup}>
 									<h2>Vill du starta denna lista?</h2>
-									<input type={"button"} value={"starta"} onClick={(list) => handleClick(list)}/>
+									<input type={"button"} value={"starta"} onClick={(list) => handleClickOpen(list)}/>
+									<input type={"button"} value={"stäng"} onClick={(list) => setPopupIsOpen(false)}/>
 								</section>
 							)
 						}
-						<SmallListObj listItem={list} key={index} handleClick={setPoputIsOpen}/>
-						</>
+							<article key={index} onClick={() => handleClickOpen(list, index)} className={styles.hiddenLists}>
+								<section className={styles.textGroup}>
+								<h3>{list.title}</h3>
+								<p>tillagd: {list._createdAt? list._createdAt.slice(0, 10) : index}</p>
+								</section>
+								<section className={styles.iconGroup}>
+									<article className={styles.nrOfTodosIcon}>
+									<p className={styles.nrTodos}>{list.nrOfTodos? list.nrOfTodos: 0}</p>
+									</article>
+								</section>
+							</article>
+						</div>
 					)
 				}) : <h3>Tomt!</h3>
 				}

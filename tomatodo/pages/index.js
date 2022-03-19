b18b2b1563a9256fc23c2207b6acfe89b30e3d29
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-19 16:24:17 +0100

    clean up

--- tomatodo/pages/index.js
+++ tomatodo/pages/index.js
@@ -3,97 +3,79 @@ import client, {
   getClient,
   usePreviewSubscription,
 } from "../lib/sanity";
-
-import React, { useContext, useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styles from '../styles/Home.module.scss';
 
 import { groq } from "next-sanity";
-import ActiveLists from "../components/ActiveLists";
-import SavedLists from "../components/SavedLists";
-import {useUpdateContext, useTodoContext} from "../context/TodoContext"
+import {useTodoContext} from "../context/TodoContext"
 import ListContainer from "../components/ListContainer";
 import Resize from '../components/Resize';
+
 export default function Post({postdata, preview}) {
 	const sectionRef = useRef();
 	const [width, setWidth] = useState(); 
 	const [open, setOpen] = useState(0); 
-
-  const router = useRouter();
-
-  const { data: posts } = usePreviewSubscription(query, {
-    initialData: postdata,
-    enabled: preview || router.query.preview !== undefined,
-  });
-
-
-  const currentState = useUpdateContext()
-  const state = useTodoContext()
+	const router = useRouter();
+  	const { data: posts } = usePreviewSubscription(query, {
+    	initialData: postdata,
+    	enabled: preview || router.query.preview !== undefined,
+	});
+ 	const state = useTodoContext()
  
+	useEffect(() => {
+		state.setInitialFetch(posts);
+		setWidth(sectionRef.current.clientWidth);
+  	}, [])
 
-  useEffect(() => {
-	state.setInitialFetch(posts);
-	setWidth(sectionRef.current.clientWidth);
-  }, [])
-
-
-
-  const activeLists = posts.allTodoLists.filter(x => x.numberOfNotChecked > 0 || x.nrOfTodos === 0);
+	const activeLists = posts.allTodoLists.filter(x => x.numberOfNotChecked > 0 || x.nrOfTodos === 0);
 	const savedLists = posts.allTodoLists.filter(x => x.saved && x.numberOfNotChecked === 0);
 	const tomatoList = posts.tomatoLibrary;
 
+  	return (
+		<div className={styles.container}>
+			<div className={styles.wrapper} ref={sectionRef}>	
+				<Resize setWidth={setWidth} width={width} sectionRef={sectionRef} />
+				{width > 600 ?( <>
+					<ListContainer key='tomatoListLg' itemType={'tomater'} setOpen={setOpen} open={open} page={'home'} list={tomatoList} setAddListFormIsVisible />
+					<ListContainer key='currentListLg' itemType={'pågående listor'} setOpen={setOpen} open={open} page={'home'} index={2} list={activeLists} setAddListFormIsVisible />
+					<ListContainer key='savedListsLg' itemType={'sparade listor'} setOpen={setOpen} open={open} page={'home'} list={savedLists} setAddListFormIsVisible />
+				</> ) : ( <> 
+					<ListContainer key='tomatoList' itemType={'tomater'} setOpen={setOpen} open={open} page={'home'} list={tomatoList} setAddListFormIsVisible />
+					<ListContainer key='savedList' itemType={'sparade listor'} setOpen={setOpen} open={open} page={'home'} list={savedLists} setAddListFormIsVisible />
+					<ListContainer key='currentList' itemType={'pågående listor'} setOpen={setOpen} open={open} page={'home'} index={2} list={activeLists} setAddListFormIsVisible />
+				</> )}
+			</div>
+		</div>
+  	);
+};
 
-
-
-  return (
-	<div className={styles.container}>
-    <div className={styles.wrapper} ref={sectionRef}>
-		
-		<Resize setWidth={setWidth} width={width} sectionRef={sectionRef} />
-		 {width > 600 ?(<>
-			<ListContainer key='tomatoListLg' itemType={'tomater'} setOpen={setOpen} open={open} page={'home'} list={tomatoList} setAddListFormIsVisible />
-			<ListContainer key='currentListLg' itemType={'pågående listor'} setOpen={setOpen} open={open} page={'home'} index={2} list={activeLists} setAddListFormIsVisible />
-		 <ListContainer key='savedListsLg' itemType={'sparade listor'} setOpen={setOpen} open={open} page={'home'} list={savedLists} setAddListFormIsVisible />
-		 </>)
-		 : 
-		(<> <ListContainer key='tomatoList' itemType={'tomater'} setOpen={setOpen} open={open} page={'home'} list={tomatoList} setAddListFormIsVisible />
-		 <ListContainer key='savedList' itemType={'sparade listor'} setOpen={setOpen} open={open} page={'home'} list={savedLists} setAddListFormIsVisible />
-		 <ListContainer key='currentList' itemType={'pågående listor'} setOpen={setOpen} open={open} page={'home'} index={2} list={activeLists} setAddListFormIsVisible /></>)
-		 }
-    </div>
-	</div>
-  );
-}
-
-const query = groq`
-{
+const query = groq`{
 	"allTodoLists": * [_type == "todoList"] | order(_createdAt desc) { 
-	  title,
-	  saved,
-	  "todos": * [_type == "todo" && todoList._ref == ^._id]{..., "slug": slug.current}+[...list]{..., "slug": slug.current},
-	  "nrOfTodos": count(* [_type == "todo" && todoList._ref == ^._id]{checked} +[...list]{checked}),
-	  'numberOfChecked': count([...list[checked]]) + count(*[_type == "todo" && todoList._ref == ^._id][checked]),
-	  'numberOfNotChecked': count([...list[!checked]]) + count(*[_type == "todo" && todoList._ref == ^._id][!checked]),
-	  ...,
-	  },
-  
-	  "tomatoLibrary": * [_type == "tomato"] | order(_createdAt desc) {
-	  title, 
-	  time, 
-	  slug,
-	  ...,
-	  "slug": slug.current,
+		title,
+		saved,
+		"todos": * [_type == "todo" && todoList._ref == ^._id]{..., "slug": slug.current}+[...list]{..., "slug": slug.current},
+		"nrOfTodos": count(* [_type == "todo" && todoList._ref == ^._id]{checked} +[...list]{checked}),
+		'numberOfChecked': count([...list[checked]]) + count(*[_type == "todo" && todoList._ref == ^._id][checked]),
+		'numberOfNotChecked': count([...list[!checked]]) + count(*[_type == "todo" && todoList._ref == ^._id][!checked]),
+		...,
+	},
+	"tomatoLibrary": * [_type == "tomato"] | order(_createdAt desc) {
+		title, 
+		time, 
+		slug,
+		...,
+		"slug": slug.current,
 	}
-  }`;
-
+}`;
 
 export async function getStaticProps({ params, preview = false }) {
 	const post = await getClient(preview).fetch(query);
   
 	return {
-	  props: {
-		postdata: post,
-		preview,
-	  },
-	  revalidate: 10,
+		props: {
+			postdata: post,
+			preview,
+	  	},
+	  	revalidate: 10,
 	};
-  }
+};

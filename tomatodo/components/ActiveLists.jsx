2c3b8b6da8114f37300e17ff0dbc9d7dca590432
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-19 15:49:38 +0100

    clean up

--- tomatodo/components/ActiveLists.jsx
+++ tomatodo/components/ActiveLists.jsx
@@ -1,42 +1,33 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import styles from '../styles/activeLists.module.scss';
 import clsx from 'clsx';
-import {useUpdateContext, useTodoContext} from "../context/TodoContext";
-
 
 const ActiveLists = ({lista, setSideListsVisible, setOpen, open, page, tomato, previewLists}) => {
-	
-	const currentState = useUpdateContext()
 	const [contentIsVisible, setContentIsVisible] = useState(page === 'home' || page === 'tomato');
-
 
 	const postTomatoTodo = async(tomatoTodo, toList) => {
 		await fetch("/api/todos/todo", {
 			method: "POST",
 			body: JSON.stringify({
-				name: tomatoTodo.name,
+				title: tomatoTodo.title,
 				description: tomatoTodo.description,
 				time: tomatoTodo.time,
 				parentRef: toList._id,
 			}),
-		  });
-		  //set
-	}
+		});
+	};
 
 	const handleClickOpen = (list, index, tomato) => {
 		if (page === "tomato") {
-		 console.log('POST TOMATO', tomato, 'TO CHOSEN TODO LIST: ', list, 'UPDATE LIST') 
-		
-		postTomatoTodo(tomato, list);
-
+			postTomatoTodo(tomato, list);
 		 } else if (page === 'todo') {
 			setOpen(index)
 		 } else if (page === 'home') {
 			console.log('go to todolists med denna som open: ', list) 
 		 }
-	}
+	};
 
 	return (
 		<div className={clsx(styles.activeLists, {
@@ -44,40 +35,49 @@ const ActiveLists = ({lista, setSideListsVisible, setOpen, open, page, tomato, p
 			[styles.homePage]: page === 'home',
 			[styles.tomatoPage]: page === 'tomato',
 			[styles.todolistPage]: page === 'todo', 
-			})}>
-			{page!== 'homeSaved' && <section className={styles.activeListsTop} onClick={page === 'todo' ? () => setContentIsVisible(!contentIsVisible): null}>
-				<h4>Mina påbörjade listor</h4><p className={styles.arrowRight} onClick={() => setSideListsVisible(false)}>&rarr;</p>
-			</section>}
+		})}>
+			{page!== 'homeSaved' && (
+			<section className={styles.activeListsTop} onClick={page === 'todo' ? () => setContentIsVisible(!contentIsVisible): null}>
+				<h4>Mina påbörjade listor</h4>
+				<p className={styles.arrowRight} onClick={() => setSideListsVisible(false)}>&rarr;</p>
+			</section>
+			)}
 			<section className={clsx(styles.contentBox, {[styles.previewLists]: previewLists===true})}>
-			{page!== 'homeSaved' && <Link href="/mina-todos" passHref>
-			<p className={styles.link}>gå till mina todos</p>
-			</Link>}
-				{
-				lista ? lista.map((list, index) => {
-					return (
-						open !== index && (
-							<article key={index} onClick={() => handleClickOpen(list, index, tomato)} className={(styles.hiddenLists)}>
-								<section className={styles.textGroup}>
-								<h3>{list.title}</h3>
-								<p>tillagd: {list._createdAt? list._createdAt.slice(0, 10) : index}</p>
-								</section>
-								<section className={styles.iconGroup}>
-									<article className={styles.nrOfTodosIcon}>
-									<p className={styles.nrTodos}>{list.nrOfTodos? list.nrOfTodos: 0}</p>
-									</article>
-									<article className={styles.nrOfNotCheckedIcon}>
-									<p className={styles.nrNotChecked}>{list.numberOfNotChecked? list.numberOfNotChecked: 0}</p>
-									</article>
-									<article className={styles.nrOfCheckedIcon}>
-									<p className={styles.nrChecked}>{list.numberOfChecked? list.numberOfChecked: 0}</p>
-									</article>
-								</section>
-							</article>
+				{page!== 'homeSaved' && (
+					<Link href="/mina-todos" passHref>
+						<p className={styles.link}>gå till mina todos</p>
+					</Link>
+				)}
+				{lista ? 
+					lista.map((list, index) => {
+						return (
+							open !== index && (
+								<article key={index} 
+									onClick={() => handleClickOpen(list, index, tomato)} 
+									className={(styles.hiddenLists)}
+								>
+									<section className={styles.textGroup}>
+										<h3>{list.title}</h3>
+										<p>tillagd: {list._createdAt? list._createdAt.slice(0, 10) : index}</p>
+									</section>
+									<section className={styles.iconGroup}>
+										<article className={styles.nrOfTodosIcon}>
+											<p className={styles.nrTodos}>{list.nrOfTodos? list.nrOfTodos: 0}</p>
+										</article>
+										<article className={styles.nrOfNotCheckedIcon}>
+											<p className={styles.nrNotChecked}>{list.numberOfNotChecked? list.numberOfNotChecked: 0}</p>
+										</article>
+										<article className={styles.nrOfCheckedIcon}>
+											<p className={styles.nrChecked}>{list.numberOfChecked? list.numberOfChecked: 0}</p>
+										</article>
+									</section>
+								</article>
+							)
 						)
-					)
-				}) : <h3>Tomt!</h3>
+					}) 
+					: <h3>Tomt!</h3>
 				}
-				</section>
+			</section>
 		</div>
 	)
 };

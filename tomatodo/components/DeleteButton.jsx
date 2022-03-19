c00aa2026cd68bf5fb1bcb1aba27e63bf01f2d4a
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-03-19 15:38:08 +0100

    clean up code

--- tomatodo/components/DeleteButton.jsx
+++ tomatodo/components/DeleteButton.jsx
@@ -1,5 +1,5 @@
 import styles from '../styles/deleteBtn.module.scss';
-import {useUpdateContext, useTodoContext} from "../context/TodoContext";
+import {useTodoContext} from "../context/TodoContext";
 import clsx from 'clsx';
 import { useState } from 'react';
 
@@ -9,60 +9,60 @@ const DeleteButton = ({color, listItem, size}) => {
 	const fetchAllLists = state.fetchTodos;
 
 	const handleDelete = async (listItem) => {
-		console.log('deleteklick:',listItem.title, listItem._id, listItem._type, 'item', listItem)
-
+		//delete todo
 		if (listItem._type === 'todo') {
 			await fetch("/api/todos/todo", {
 				method: "DELETE",
 				body: listItem._id,
-			  })
-			  
-		.then(console.log('posted'))
-		.catch(error => {
-			console.log('error:', error);
-		})
-		fetchAllLists();
-		
-			 
-			  ;
+			})
+			.then(console.log('posted'))
+			.catch(error => {
+				console.log('error:', error);
+			})
+			fetchAllLists();			 
+		//delete todo-list
 		} else if (listItem._type === 'todoList') {
 			await fetch("/api/todos/todolist", {
 				method: "DELETE",
 				body: listItem._id,
-			  })
-		.then(console.log('posted'))
-		.catch(error => {
-			console.log('error:', error);
-		})
-		fetchAllLists();
-		
-			  ;
+			})
+			.then(console.log('posted'))
+			.catch(error => {
+				console.log('error:', error);
+			})
+			fetchAllLists();
 		} else if (listItem._type === 'tomato') {
+		//delete tomato
 			await fetch("/api/tomatoes/tomato", {
 				method: "DELETE",
 				body: listItem._id,
-			  })
-		fetchAllLists();
+			})
+			fetchAllLists();
 		}
 		setShowWarning(false);
-	}
+	};
 
-	return (<>
-		<div onClick={() => setShowWarning(!showWarning)} className={clsx(styles.deleteBtn, {[styles.smallOrange]: color === 'orange', [styles.smallBlue]: color === 'blue', [styles.large]: size === 'large' })} />
-		{ showWarning &&(
-			<div className={styles.deleteWarning}>
-			<h2>Vill du ta bort {listItem.title}?</h2>
-			<div className={styles.btnContainer}>
-				<input className={clsx(styles.warningBtn,styles.delete)} type={"button"} onClick={() => handleDelete(listItem)} value={'ta bort'} />
-				<input className={clsx(styles.warningBtn, styles.close)} type={"button"} onClick={() => setShowWarning(false)} value={'ångra'}  />
-			</div>
-		</div>)}
+	return (
+		<>
+			<div onClick={() => setShowWarning(!showWarning)} 
+				className={clsx(styles.deleteBtn, {
+					[styles.smallOrange]: color === 'orange', 
+					[styles.smallBlue]: color === 'blue', 
+					[styles.large]: size === 'large' 
+				})} 
+			/>
+			{showWarning && (
+				<div className={styles.deleteWarning}>
+					<h2>Vill du ta bort {listItem.title}?</h2>
+					<div className={styles.btnContainer}>
+						<input className={clsx(styles.warningBtn,styles.delete)} type={"button"} onClick={() => handleDelete(listItem)} value={'ta bort'} />
+						<input className={clsx(styles.warningBtn, styles.close)} type={"button"} onClick={() => setShowWarning(false)} value={'ångra'}  />
+					</div>
+				</div>
+			)}
 		</>
 	)
-}
-
-
-
+};
 
 export default DeleteButton;
 

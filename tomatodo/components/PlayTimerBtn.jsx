f61d9d208d2849384e235f04ade7474c24be02a1
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-05-06 12:07:55 +0200

    remove link-element, add router-function

--- tomatodo/components/PlayTimerBtn.jsx
+++ tomatodo/components/PlayTimerBtn.jsx
@@ -1,32 +1,31 @@
 import styles from '../styles/playBtn.module.scss';
-import Link from 'next/link';
 import clsx from 'clsx';
 import { useUpdateContext } from "../context/TodoContext";
+import { useRouter } from 'next/router';
 
-const PlayBtn = ({listItem, size, content, styleChecked}) => {
+const PlayBtn = ({ listItem, size, content, styleChecked }) => {
 	const currentState = useUpdateContext();
+	const router = useRouter();
+	
+	const click = () => {
+		currentState.setCountdownItem(listItem);
+		router.push('/timer-countdown');
+	};
 
 	return (
-		<aside className={styles.todoTimeSection} onClick={() => currentState.setCountdownItem(listItem)} >
+		<aside className={styles.todoTimeSection} onClick={() => click()} >
 			{
 				listItem.time || currentState.countdownItem ? (
-					<>
-						<Link
-							href={'/timer-countdown'}
-							passHref
-						>
-							<button 
-								className={clsx(styles.playBtn, {
-									[styles.orange]: listItem._type ==="tomato",
-									[styles.green]: listItem._type !=="tomato" && styleChecked,
-									[styles.grey]: listItem._type !=="tomato" && !styleChecked,
-									[styles.orangeLg]: size === 'large',
-									[styles.regularBtn]: content && content.length>0, 
-								})} 
-								value={`${content}`}
-							/>
-						</Link>
-					</>
+					<button 
+						className={clsx(styles.playBtn, {
+							[styles.orange]: listItem._type ==="tomato",
+							[styles.green]: listItem._type !=="tomato" && styleChecked,
+							[styles.grey]: listItem._type !=="tomato" && !styleChecked,
+							[styles.orangeLg]: size === 'large',
+							[styles.regularBtn]: content && content.length>0, 
+						})} 
+						value={`${content}`}
+					/>
 				) : (
 					<article className={styles.playBtnPlaceholder} /> 
 				)

54d2bfaf616fd77d740f080b6141873ced3f9e7c
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-05-07 20:56:15 +0200

    add volume controller, change page prop to router path

--- tomatodo/components/Time/TomatoBtnContainers.jsx
+++ tomatodo/components/Time/TomatoBtnContainers.jsx
@@ -1,11 +1,42 @@
 import clsx from 'clsx';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles/btnContainer.module.scss';
+import { useRouter } from "next/router";
 
-const TomatoBtnContainers = ({page, functions, states }) => {
+const TomatoBtnContainers = ({ functions, states }) => {
+	const router = useRouter();
+	const [showVolume, setShowVolume] = useState(false);
+	const [volumeBoxes, setVolumBoxes] = useState([]);
+
+	const setVolume = (operation, volume) => {
+		console.log('volume', volume, Number(volume.toFixed(1)), typeof volume, typeof Number(volume.toFixed(1)))
+		if (operation === 'add') {
+			volume < 1 ? functions.setVolume((Number((volume + 0.1).toFixed(1)))) : null; 
+		} else if (operation === 'subtract') {
+			volume > 0 ? functions.setVolume((Number((volume - 0.1).toFixed(1)))) : null; 
+		}
+	};
+
+	const toggleSound = () => {
+		functions.setVolume(states.volume < 0.1 ? 1 : 0);
+	};
+
+	const volumeArray = [];
+
+	useEffect(() => {
+		if (states && states.volume) {
+		for (let index = 0; index < states.volume*10; index++) {
+			volumeArray.push(index + 1);
+		};
+		setVolumBoxes(volumeArray);
+		}
+	}, [states]);
+
+
+
 	return (
 	<>
-	{ page === 'none' ?
+	{ router.pathname === '/timer' ?
 	(
 		<section className={styles.buttonContainer}>
 			<article className={clsx(styles.timerBtn, styles.playBtn, styles.disabled)}/>
@@ -15,7 +46,7 @@ const TomatoBtnContainers = ({page, functions, states }) => {
 		</section>
 	) : (
 		<section className={styles.buttonContainer}>
-			{page === "stopwatch" ? 
+			{router.pathname === '/stopwatch' ? 
 			<article 
 				className={clsx(styles.timerBtn, styles.playBtn, {[styles.disabled]: states.isCounting})} 
 				onClick={() => functions.start()}
@@ -23,7 +54,7 @@ const TomatoBtnContainers = ({page, functions, states }) => {
 			<article className={clsx(styles.timerBtn, styles.playBtn, {[styles.disabled]: states.isRunning || !states.isInitialized})} 
 			onClick={!states.isRunning && states.isInitialized ? () => functions.handlePlay(): null} />
 			}
-			{page === "stopwatch" ?
+			{router.pathname === '/stopwatch' ?
 			<article 
 				className={clsx(styles.timerBtn, styles.pauseBtn, {[styles.disabled]: !states.isCounting || states.currentTime < 1})} 
 				onClick={() => functions.pause()} 
@@ -36,7 +67,7 @@ const TomatoBtnContainers = ({page, functions, states }) => {
 					<article className={clsx(styles.timerBtn, styles.stopBtn, {[styles.disabled]: !states.isInitialized})} onClick={() => functions.handleStop()} />
 				)
 		}
-			{page === "stopwatch" ?
+			{router.pathname === '/stopwatch' ?
 			<article 	
 				className={clsx(styles.timerBtn, styles.stopBtn, {[styles.disabled]: states.currentTime < 1})} 
 				onClick={() => functions.reset()} 
@@ -44,12 +75,37 @@ const TomatoBtnContainers = ({page, functions, states }) => {
 			<article className={clsx(styles.timerBtn, styles.restartBtn, {[styles.disabled]: !states.isInitialized})} onClick={() => functions.restart()} />
 		}
 			
-			{page === "stopwatch" ? 
+			{router.pathname === '/stopwatch' ? 
 			<article 
 				className={clsx(styles.timerBtn, styles.saveBtn, {[styles.disabled]: states.isCounting || states.currentTime < 1})}  
 				onClick={() => functions.setFormIsVisible(true)}
 			/>:
-			<article className={clsx(styles.timerBtn, {[styles.soundOffBtn]: !states.soundOn, [styles.soundOnBtn]: states.soundOn})} onClick={() => functions.setSoundOn(!states.soundOn)} />
+			<>
+				<article className={clsx(styles.timerBtn, {[styles.soundOffBtn]: states.volume < 0.1, [styles.soundOnBtn]: states.volume > 0})} onClick={() => toggleSound()} />
+					{showVolume && (
+						<>
+							<div className={styles.volumeSection}>
+								<div className={styles.volumGraph}>
+								{
+								volumeBoxes.map((x) => {
+									return (<article key={x} style={{height: `${x*10}%`}} 
+									className={clsx(styles.volumBox, {
+										[styles.volumeBlue]: states.volume > 0,
+										[styles.volumeOrange]: states.volume > 0.4,
+										[styles.volumePink]: states.volume > 0.7
+									})} />)
+								})}
+								</div>
+								<div className={styles.volumeBtnGroup}>
+							<button className={styles.volumController} onClick={() =>  setVolume('subtract', states.volume)}> - </button>
+							<p className={styles.volumePercent}>{`${states.volume * 100}%`}</p>
+							<button className={styles.volumController} onClick={() => setVolume('add', states.volume)}> + </button>
+							</div>
+							</div>
+						</>		
+				)}
+				<button className={styles.showVolumeBtn} onClick={() => setShowVolume(!showVolume)}>{showVolume ? 'dölj':'ändra volym'}</button>
+			</>
 		}	
 		</section>)
 

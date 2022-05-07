c376700dec4a4857287637df22164b30817dc457
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-05-07 20:09:14 +0200

    update numbers to not affect each other when counting

--- tomatodo/components/NumberFormat.jsx
+++ tomatodo/components/NumberFormat.jsx
@@ -1,3 +1,5 @@
+import styles from '../styles/numberFormat.module.scss';
+
 const NumberFormat = ({ milliSeconds, text, showSecs, styling }) => {
 
 	let time = Math.round(Number(milliSeconds)/1000);
@@ -13,11 +15,20 @@ const NumberFormat = ({ milliSeconds, text, showSecs, styling }) => {
 		: timeForm = `${hours}:${minutes}`
 
 	return (
-		<h2 style={styling}>
+		<>
+		{text || !showSecs ? (
+			<h2 style={styling}>
 			{time > 0 && 
 			`${text? text : ''} ${timeForm}`
 			}
 		</h2>
+		) : (
+			<div className={styles.numbersContainer}>
+				<h2 className={styles.number}>{hours}</h2><h2> : </h2><h2 className={styles.number}>{minutes}</h2><h2> : </h2><h2 className={styles.number}>{seconds}</h2>
+			</div>
+
+		)}
+		</>
 	);
 };
 

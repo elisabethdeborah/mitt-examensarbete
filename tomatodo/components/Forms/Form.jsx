ccaf8b7e9de3a66c4046bc575817568965998b14
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-05-08 23:51:26 +0200

    add userid to post requests

--- tomatodo/components/Forms/Form.jsx
+++ tomatodo/components/Forms/Form.jsx
@@ -4,20 +4,24 @@ import clsx from "clsx";
 import {useUpdateContext, useTodoContext} from "../../context/TodoContext";
 import TimeInput from "./TimeInput";
 import { validateName, submitClick, calculateTime } from "./formFunctions";
+import { useUserStore } from '../../context/UserStore';
+import { useRouter } from "next/router";
 
-const Form = ({ objectType, method, currentListDocId, defaultTime, page }) => {
-	
-	const state = useTodoContext();
+const Form = ({ objectType, method, currentListDocId, defaultTime }) => {
+	const router = useRouter();
+	const todoState = useTodoContext();
 	const currentState = useUpdateContext();
-	const fetchAllLists = state.fetchTodos;
+	const fetchAllLists = todoState.fetchTodos;
+	const { state, dispatch } = useUserStore();
+	const { userInfo } = state;
 
 	const [userInputName, setUserInputName] = useState(currentState.currentItem? currentState.currentItem.title: '');
 	const [userInputText, setUserInputText] = useState(currentState.currentItem && currentState.currentItem.description ? currentState.currentItem.description : '');
 	const [userInputTime, setUserInputTime] = useState(0);
 	const [errMessage, setErrMessage] = useState('');
 	const [inputTime, setInputTime] = useState(0);
-	const [body, setBody] = useState({title: userInputName, description: userInputText, time: inputTime});
-	
+	const [body, setBody] = useState({title: userInputName, description: userInputText, time: inputTime, userId: userInfo._id});
+
 	let header;
 	
 	if (method === "PUT" && currentState.currentItem) {
@@ -33,10 +37,14 @@ const Form = ({ objectType, method, currentListDocId, defaultTime, page }) => {
 	}, []);
 
 	useEffect(() => {
-		setTimeout(() => {
-			currentState.setOverlay(true);
-		}, 10);
-		return () => currentState.setOverlay(false);
+		if (router.pathname !== '/mina-tomater' && router.pathname !== '/mina-sparade-listor') {
+			setTimeout(() => {
+				currentState.setOverlay(true);
+			}, 10);
+			return () => {
+				currentState.setOverlay(false);
+			};
+		};
 	}, []);
 
 	useEffect(() => {
@@ -48,28 +56,41 @@ const Form = ({ objectType, method, currentListDocId, defaultTime, page }) => {
 
 	const postFetch = () => {
 		fetchAllLists();
-		if (page !== 'archive')  {
-		setBody({title: userInputName, description: userInputText, time: inputTime});
-		setUserInputName('');
-		setUserInputTime(0);
-		setUserInputText('');
-		setErrMessage('');
-		currentState.handleGoBack();
-		currentState.setCurrentItem(null);
+		if (router.pathname !== '/mina-tomater' && router.pathname !== '/mina-sparade-listor')  {
+			setBody({title: userInputName, description: userInputText, time: inputTime, user: userInfo._id});
+			setUserInputName('');
+			setUserInputTime(0);
+			setUserInputText('');
+			setErrMessage('');
+			currentState.handleGoBack();
+			currentState.setCurrentItem(null);
+			setTimeout(() => {
+				currentState.setOverlay(false);
+			}, 400);
 		};
-		setTimeout(() => {
-			currentState.setOverlay(false);
-		}, 600);
 	};
 
 	const handleSubmit = () => {
 		const checkValid = validateName(body);
 		if (checkValid === "valid") {
-			submitClick(objectType, body, method, state);
+			if (objectType === 'tomato' || objectType === 'todoList') {
+				submitClick(objectType, body, method, state);
+			} else {
+				submitClick(objectType, body, method, state);
+			}
+			
 			postFetch(currentState);
 		} else {
 			setErrMessage(checkValid);
-		}
+		};
+	};
+
+	const closeForm = () => {
+		if (router.pathname !== '/mina-tomater' && router.pathname !== '/mina-sparade-listor') {
+			currentState.handleGoBack()
+		} else {
+			currentState.handleGoBack()
+		};
 	};
 
 	return (
@@ -80,57 +101,59 @@ const Form = ({ objectType, method, currentListDocId, defaultTime, page }) => {
 				[styles.todoListFormContainer]: objectType === 'todoList',
 			})}
 		>	
-			{
-				objectType === 'tomato' && page !== 'archive' && (
-				<aside className={styles.smallTomato} />
-				)
-			}
-				<h1 className={styles.formHeader}>
-					{header? `Redigera\n ${header}`: `Ny ${objectType}`}
-				</h1>
+		{
+			objectType === 'tomato' && router.pathname !== '/mina-tomater' && (
+			<aside className={styles.smallTomato} />
+			)
+		}
+			<h1 className={styles.formHeader}>
+				{header? `Redigera\n ${header}`: `Ny ${objectType}`}
+			</h1>
+		{
+			objectType !== 'todoList' && currentState.currentItem && currentState.currentItem.title ? (
+			<input 
+				type="text" 
+				className={clsx(styles.input, styles.textInput)} 
+				value={body.title} 
+				onChange={(e) => setBody((body) => ({...body, title: e.target.value}))}
+			/>
+			) : (
+			<input 
+				type="text" 
+				className={clsx(styles.input, styles.textInput)} 
+				placeholder={`Namn på ${objectType}`} 
+				onChange={(e) => setBody((body) => ({...body, title: e.target.value}))}
+			/>)
+		}
+		{
+			objectType !== 'todoList' && (
+				<>
 				{
-					objectType !== 'todoList' && currentState.currentItem && currentState.currentItem.title ? (
-					<input 
-						type="text" 
-						className={clsx(styles.input, styles.textInput)} 
-						value={body.title} 
-						onChange={(e) => setBody((body) => ({...body, title: e.target.value}))}
-					/>
-					) : (
-					<input 
-						type="text" 
-						className={clsx(styles.input, styles.textInput)} 
-						placeholder={`Namn på ${objectType}`} 
-						onChange={(e) => setBody((body) => ({...body, title: e.target.value}))}
-					/>)
+					currentState.currentItem && currentState.currentItem.description ? (
+						<input 
+							type="text" 
+							className={clsx(styles.input, styles.textInput)} 
+							value={body.description} 
+							onChange={(e) => setBody((body) => ({...body, description: e.target.value}))}
+						/>) : (
+						<input 
+							type="text" 
+							className={clsx(styles.input, styles.textInput)} 
+							placeholder={"Beskrivning"} 
+							onChange={(e) => setBody((body) => ({...body, description: e.target.value}))}
+						/>)
 				}
-			{
-				objectType !== 'todoList' && (
-					<>
-					{
-						currentState.currentItem && currentState.currentItem.description ? (
-							<input 
-								type="text" 
-								className={clsx(styles.input, styles.textInput)} 
-								value={body.description} 
-								onChange={(e) => setBody((body) => ({...body, description: e.target.value}))}
-							/>) : (
-							<input 
-								type="text" 
-								className={clsx(styles.input, styles.textInput)} 
-								placeholder={"Beskrivning"} 
-								onChange={(e) => setBody((body) => ({...body, description: e.target.value}))}
-							/>)
-						}
-						<TimeInput userInputTime={userInputTime} setUserInputTime={setUserInputTime} setBody={setBody} body={body} inputTime={inputTime} />
-					</>
-				)
-			}
+					<TimeInput userInputTime={userInputTime} setUserInputTime={setUserInputTime} setBody={setBody} body={body} inputTime={inputTime} />
+				</>
+			)
+		}
+			<div className={styles.errMessageContainer}>
+				<p className={styles.errMessage}>{errMessage}</p>
+			</div>
 			<div className={styles.btnContainer}>
 				<input type={"button"} className={styles.closeForm} value="Ångra" onClick={() => currentState.handleGoBack()} />
 				<input type={"button" }className={styles.addBtn} value="Lägg till" onClick={() => handleSubmit()} />
 			</div>
-			{errMessage}
 		</section>
 	);
 };

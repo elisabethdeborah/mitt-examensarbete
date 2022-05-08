18e3ddf2cdedd781b7a5d4b37d10d70fee666d50
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2022-05-08 23:11:35 +0200

    add resize on mount

--- tomatodo/components/Resize.jsx
+++ tomatodo/components/Resize.jsx
@@ -3,12 +3,21 @@ import React, { useEffect } from "react";
 const Resize = ({ width, setWidth, sectionRef }) => {
 	useEffect(() => {
 		const onResize = () => {
-			const newWidth = sectionRef.current.clientWidth;
+			if (sectionRef && sectionRef.current) {
+				const newWidth = sectionRef.current.clientWidth;
 			setWidth(newWidth);
+			};
 		};
     	window.addEventListener("resize", onResize);
 		return () => window.removeEventListener("resize", onResize);
 	});
+	
+	useEffect(() => {
+		if (sectionRef && sectionRef.current) {
+			const newWidth = sectionRef.current.clientWidth;
+		setWidth(newWidth);
+		};
+	}, []);
 	return null;
 };
 
